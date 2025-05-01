fc935a077dcd6829dae4ce49e9cc86799c02cbc6
2453146 <168549311+Soldier-of-God@users.noreply.github.com>
2025-05-01 09:08:10 +0200

    Update jobs.js to have router to updateStatus

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,5 +4,6 @@ const jobController = require('../controllers/jobConttroller');
 
 router.get('/', jobController.showAvailableJobs);
 router.post('/', jobController.addJob);
+router.post('/updateStatus',jobController.updateStatus);
 
 module.exports = router;
