d5f0e9d1c93f9bae218c5814d7898a42564a9544
bigJay101 <jtshibumbu12@gmail.com>
2025-05-07 09:46:40 +0200

    Removed lanceDB folder

--- lanceDB/routes/jobs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jobController = require('../controllers/jobConttroller');
-
-router.get('/', jobController.showAvailableJobs);
-router.post('/', jobController.addJob);
-router.post('/updateStatus',jobController.updateStatus);
-
-module.exports = router;
--- lanceDB/routes/lancers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const lancerController = require('../controllers/lancerController');
-
-router.post('/', lancerController.addLancer);
-
-module.exports = router;
