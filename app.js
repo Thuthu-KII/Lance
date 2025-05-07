93adfe75e263efe0d2df89435c727f5d2977e867
bigJay101 <jtshibumbu12@gmail.com>
2025-05-07 10:22:20 +0200

    updated how the body is parsed

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 const jobRoutes = require('./routes/jobs');
 const clientRoutes = require('./routes/clients');
 const lancerRoutes = require('./routes/lancers');
-const applyRoutes = require('./routes/apply')
+const applyRoutes = require('./routes/apply');
 
 const app = express();
-app.use(bodyParser.json());
+
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/jobs', jobRoutes);
 app.use('/clients', clientRoutes);
 app.use('/lancers', lancerRoutes);
-app.use('/apply',applyRoutes);
-app.use(cors());
+app.use('/apply', applyRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
