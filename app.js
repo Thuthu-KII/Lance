9ebaa27603688935df73cfcabdb7086c3bb76994
bigJay101 <jtshibumbu12@gmail.com>
2025-05-25 13:32:11 +0200

    made secure true

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production',
+    secure: true, //process.env.NODE_ENV === 'production',
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
